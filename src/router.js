138d58ce9e4f0fa75c3c0eeaa9489ae923d72744
段佳维 <554343346@qq.com>
2019-05-21 19:01:26 +0800

    配置路由懒加载

--- src/router.js
+++ src/router.js
@@ -1,38 +1,41 @@
 // import Vue from 'vue'
 // import Router from 'vue-router'
-import Home from './views/Home.vue'
-import One from './views/One.vue'
-import Two from './views/Two.vue'
-import Three from './views/Three.vue'
+// import Home from './views/Home.vue'
+// import One from './views/One.vue'
+// import Two from './views/Two.vue'
+// import Three from './views/Three.vue'
 
 // Vue.use(Router)
-
+const home = resolve => require(['./views/Home.vue'],resolve)
+const one = resolve => require(['./views/One.vue'],resolve)
+const two = resolve => require(['./views/Two.vue'],resolve)
+const three = resolve => require(['./views/Three.vue'],resolve)
 export default new VueRouter({
   routes: [
       {
           path: '',
-          component: Home,
+          component: home,
           children: [
               {
                   path: '',
                   name: 'one',
-                  component: One
+                  component: one
               },
               {
               path: 'one',
               name: 'one',
-              component: One
+              component: one
           },
               {
                   path: 'two',
                   name: 'two',
-                  component: Two
+                  component: two
               }
               ,
               {
                   path: 'three',
                   name: 'three',
-                  component: Three
+                  component: three
               }
           ]
       }
