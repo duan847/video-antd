81b8c4ce76ccbd1a108b22cf066630e195f356f3
554343346@qq.com <duan123>
2019-05-27 23:18:07 +0800

    修改title

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -5,7 +5,7 @@
                 <img src="../assets/logo.png" class="logo" alt="首页"/>
             </a-col>
             <a-col :xs="14" :sm="11" :md="13" :lg="9" :xl="6">
-                <a-input-search placeholder="搜索 电影名/电视剧名/" @search="onSearch"/>
+                <a-input-search placeholder="搜索 电影名/电视剧名" @search="onSearch"/>
             </a-col>
             <a-col :xs="4" :sm="6" :md="4" :lg="3" :xl="9">
                 <span style="float:right">
@@ -23,6 +23,7 @@
         methods: {
             home(){
                 this.$router.push({name: "home"})
+                document.title = "看片-免费/无广告"
             },
             onSearch (text) {
                 if(text) {
